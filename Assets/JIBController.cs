660314ab1ecfd1604b7064f880584ca36b898c6e
liuqi314 <w0490374@nscc.ca>
2025-02-06 16:12:06 -0400

    Add outline

--- Assets/JIBController.cs
+++ Assets/JIBController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class JIBController : MonoBehaviour
 {
     public Animator animator;             // Animator for the jack-in-the-box animation
-    /*public ParticleSystem confetti;*/       // Particle system for the confetti
-    /*public float confettiDelay = 2.0f;*/    // Time delay before confetti plays (2 seconds after animation starts)
+    public ParticleSystem confetti;      // Particle system for the confetti
+    public float confettiDelay = 2.0f;   // Time delay before confetti plays (2 seconds after animation starts)
     private bool isPlaying = false;
     public AudioSource musicBox;
 
@@ -16,19 +16,19 @@ public class JIBController : MonoBehaviour
         {
             animator.Play("JackInTheBox");   // Replace with the actual animation clip name
             isPlaying = true;
-            /*StartCoroutine(PlayConfettiWithDelay(confettiDelay));*/ // Start coroutine to delay confetti
+            StartCoroutine(PlayConfettiWithDelay(confettiDelay)); // Start coroutine to delay confetti
             musicBox.Play();
         }
     }
 
     // Coroutine to delay confetti trigger
-    /*
+    
     IEnumerator PlayConfettiWithDelay(float delay)
     {
         yield return new WaitForSeconds(delay); // Wait for the specified delay
         confetti.Play();                        // Play the confetti particle system
     }
-    */
+    
 }
 
 
